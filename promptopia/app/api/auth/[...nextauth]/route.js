0a498478c3e54fbaca466d6e7078c32ee3e1d0e5
glenmiracle18 <m.bonyu@alustudent.com>
2023-11-19 13:58:59 +0200

    use authentication

---
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google'
+import { signIn } from "next-auth/react";
+import { connectToDB } from "@utils/database";
+// console.log({
+//     clientId: process.env.GOOGLE_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+// })
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    async session({ session }) {
+
+    },
+    async signIn({ profile }) {
+        try {
+            await connectToDB();
+
+            //check if a use already exists
+
+            // if not, create new user
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+})
+
+export { handler as GET, handler as POST };
