ad6fb9aaa633ca515e6f0cbee448ec3be3fec3e7
glenmiracle18 <m.bonyu@alustudent.com>
2023-11-19 10:26:20 +0200

    done

--- promptopia/components/Nav.jsx
+++ promptopia/components/Nav.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
   // logIn/SignIn logic with auth providers like google
   const isUserLoggedIn = true;
   const [providers, setProviders ] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
     const setProviders = async () => {
@@ -57,15 +58,68 @@ const Nav = () => {
                 height={37}
                 className='rounded-full'
                 alt='profile'
+                
               />
             </Link>
           </div>
         ): ( 
           // this is condition for when the isUserLoggedIn is false
           <>
-
+            {providers && 
+              Object.values(providers).map((provider) => (
+                  <button type='button' key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                  >
+                    Sign In
+                  </button>
+                )
+              )
+            }
           </>
         )}
+
+        {/* mobile navigation */}
+        <div className="sm:hidden flex relative">
+          {isUserLoggedIn ? (
+            <div className="flex">
+              <Image 
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+                onClick={() => setToggleDropdown((prev) => 
+                  !prev)}
+              />
+              {toggleDropdown && (
+                <div className="dropdown">
+                  <Link 
+                    href='/profile'
+                    className='dropdown_link'
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    My Profile
+                  </Link>
+                </div>
+              )}
+            </div>
+          ): (
+            <>
+            {providers && 
+              Object.values(providers).map((provider) => (
+                  <button type='button' key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                  >
+                    Sign In
+                  </button>
+                )
+              )
+            }
+          </>
+          )}
+        </div>
       </div>
     </nav>
   )
